90c873634892aa7aed96abb3c7e76eb54fcb6802
Zach Keeffe <zachary.keeffe@my.jcu.edu.au>
2021-04-18 17:54:23 +1000

    whitespace corrected to remove formatting errors guitar files including tests added prac_06 main work completed

--- prac_02/string_formatting_examples.py
+++ prac_02/string_formatting_examples.py
@@ -9,11 +9,11 @@ year = 1922
 cost = 16035.4
 
 # The ‘old’ manual way to format text with string concatenation:
-print("My guitar: " + name + ", first made in " + str(year))
+print("My guitar.py: " + name + ", first made in " + str(year))
 
 # A better way - using str.format():
-print("My guitar: {}, first made in {}".format(name, year))
-print("My guitar: {0}, first made in {1}".format(name, year))
+print("My guitar.py: {}, first made in {}".format(name, year))
+print("My guitar.py: {0}, first made in {1}".format(name, year))
 print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
 
 # Formatting currency (grouping with comma, 2 decimal places):
