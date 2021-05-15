1a3e4baf7ac2ea8093ae7206baf7759b2ef03938
Zach Keeffe <zachary.keeffe@my.jcu.edu.au>
2021-05-15 15:27:34 +1000

    - implement fix for file cleanup - file cleanup operates, but not as efficient as possible

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -24,17 +24,25 @@ def main():
             new_name = os.path.join(directory_name, get_fixed_filename(filename))
             os.rename(full_name, new_name)
 
+
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    for i in enumerate(new_name):
-        print(i)
+    main_name = new_name.split('.')[0]
+    extension = new_name.split('.')[-1]
+    letter_to_check = ""
+    special_characters = ["'", ".", "_", "(", ")"]
+    alt_name = ""
+    for i in enumerate(main_name):
+        if not i[0] == 0 and letter_to_check == letter_to_check.lower() and not i[1] == i[1].lower() and not letter_to_check in special_characters:
+            alt_name += "_" + i[1]
+        else:
+            alt_name += i[1]
+        letter_to_check = i[1]
+    main_name = alt_name
 
+    new_name = "{}.{}".format(main_name, extension)
     return new_name
-
-
-
-
 
 
 main()
