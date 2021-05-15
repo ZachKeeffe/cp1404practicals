99b4729d2806c9f9c79eb5cd1a43b119041e02e1
Zach Keeffe <zachary.keeffe@my.jcu.edu.au>
2021-05-15 13:15:20 +1000

    - remove TODO and commented out code

--- prac_09/os_demos.py
+++ prac_09/os_demos.py
@@ -2,12 +2,11 @@
 CP1404/CP5632 Practical
 Demos of various os module examples
 """
-import shutil
 import os
 
 
 def main():
-    """Demo os module functions."""
+    """Demo of os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
 
     # Change to desired directory
@@ -16,8 +15,8 @@ def main():
     # Print a list of all files in current directory
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
-    # Make a new directory if one does not exist, move on if it does exist
-
+    # Make a new directory
+    # The next time you run this, it will crash if the directory exists
     try:
         os.mkdir('temp')
     except FileExistsError:
@@ -31,13 +30,6 @@ def main():
 
         new_name = get_fixed_filename(filename)
         print("Renaming {} to {}".format(filename, new_name))
-
-        # TODO: Try these options one at a time
-        # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
-
-        # Option 2: move file to new place, with new name
-        shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
@@ -55,13 +47,10 @@ def demo_walk():
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
 
-        # TODO: add a loop to rename the files
         for filename in filenames:
             full_name = os.path.join(directory_name, filename)
             new_name = os.path.join(directory_name, get_fixed_filename(filename))
             os.rename(full_name, new_name)
 
 
-
-# main()
-demo_walk()
+main()
